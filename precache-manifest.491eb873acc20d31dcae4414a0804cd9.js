eede5652f2bfc64c7dbe74004d21dce6865f858f
daricocity <daricocity@gmail.com>
2020-02-20 04:26:49 +0100

    Updates

---
+++ precache-manifest.491eb873acc20d31dcae4414a0804cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d80a44feca9dc3d693ba5c07779a29",
+    "url": "/daricocity/react-monst.git/index.html"
+  },
+  {
+    "revision": "176656d72f171d482329",
+    "url": "/daricocity/react-monst.git/static/css/main.8b1ecd77.chunk.css"
+  },
+  {
+    "revision": "8ba87e482582d8c4a939",
+    "url": "/daricocity/react-monst.git/static/js/2.cef6a132.chunk.js"
+  },
+  {
+    "revision": "176656d72f171d482329",
+    "url": "/daricocity/react-monst.git/static/js/main.832c57ba.chunk.js"
+  },
+  {
+    "revision": "2ce036f69059fec8d9ab",
+    "url": "/daricocity/react-monst.git/static/js/runtime-main.ed5b2345.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/daricocity/react-monst.git/static/media/logo.25bf045c.svg"
+  }
+]);
